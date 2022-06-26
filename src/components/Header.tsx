e2e4f9af2f3a9d10e0d3a57c057c8cd278fb4032
ZR21080178-闫志浩 <yanzhihao@zhongruigroup>
2022-06-26 15:46:01 +0800

    config-overrides.js

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
 
+import { Menu } from '@arco-design/web-react';
+const MenuItem = Menu.Item;
+const SubMenu = Menu.SubMenu;
+
 class Header extends Component {
     render() {
         return (
-            <div>
-                sss
+            <div className='menu-demo'>
+                <Menu mode='horizontal' defaultSelectedKeys={['1']}>
+                    <MenuItem
+                        key='0'
+                        style={{ padding: 0, marginRight: 38, }}
+                        disabled
+                    >
+                        <div
+                            style={{
+                                width: 80,
+                                height: 30,
+                                borderRadius: 2,
+                                background: 'var(--color-fill-3)',
+                                cursor: 'text',
+                            }}
+                        />
+                    </MenuItem>
+                    <MenuItem key='1'>Home</MenuItem>
+                    <MenuItem key='2'>Solution</MenuItem>
+                    <MenuItem key='3'>Cloud Service</MenuItem>
+                    <MenuItem key='4'>Cooperation</MenuItem>
+                </Menu>
             </div>
         );
     }
